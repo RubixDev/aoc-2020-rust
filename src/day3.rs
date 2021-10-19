0da22f121eda762a88051dec97f2d8bd53677885
RubixDev <silas.groh@t-online.de>
2021-10-19 22:25:50 +0200

    Day 3 and execution time measurement

---
+++ src/day3.rs
@@ -0,0 +1,48 @@
+struct Slope {
+    right: u8,
+    down: u8,
+}
+
+fn drive_slope(slope: Slope, map: &Vec<Vec<char>>) -> u8 {
+    let mut count: u8 = 0;
+    let mut pos_x: u16 = 0;
+    for (y, line) in map.iter().enumerate() {
+        if slope.down != 1 && y % (slope.down as usize) != 0 {
+            continue;
+        }
+        if line[(pos_x as usize) % line.len()] == '#' {
+            count += 1
+        }
+        pos_x += slope.right as u16
+    }
+    return count;
+}
+
+pub fn run() {
+    println!("--- DAY 03 ---");
+    let input: Vec<Vec<char>> = include_str!("resources/input03.txt")
+        .split('\n')
+        .map(|line| line.chars().collect())
+        .collect();
+    part1(&input);
+    println!();
+    part2(&input);
+    println!();
+}
+
+fn part1(input: &Vec<Vec<char>>) {
+    println!("{}", drive_slope(Slope { right: 3, down: 1 }, input))
+}
+
+fn part2(input: &Vec<Vec<char>>) {
+    let slopes: [Slope; 5] = [
+        Slope { right: 1, down: 1 },
+        Slope { right: 3, down: 1 },
+        Slope { right: 5, down: 1 },
+        Slope { right: 7, down: 1 },
+        Slope { right: 1, down: 2 }
+    ];
+    println!("{}", slopes.map(|slope| {
+        drive_slope(slope, input)
+    }).iter().fold(1u32, |acc, &count| acc * (count as u32) )) // .product::<_>()
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,15 @@
+use std::time;
+
 mod day1;
 mod day2;
+mod day3;
 
 fn main() {
+    let start = time::Instant::now();
+
     day1::run();
     day2::run();
+    day3::run();
+
+    println!("\n---------------\n Execution took {:?}", start.elapsed())
 }
