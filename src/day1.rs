ed3c6bf7ce92e7e02aec1118c06a1a420c776e79
RubixDev <silas.groh@t-online.de>
2021-10-19 18:42:49 +0200

    Days 1 and 2

---
+++ src/day1.rs
@@ -0,0 +1,33 @@
+pub fn run() {
+    println!("--- DAY 01 ---");
+    let input = include_str!("resources/input01.txt")
+        .split('\n')
+        .map(|num| num.parse::<u32>().unwrap())
+        .collect::<Vec<u32>>();
+    part1(&input);
+    println!();
+    part2(&input);
+    println!();
+}
+
+fn part1(input: &Vec<u32>) {
+    for (index1, num1) in input.iter().enumerate() {
+        for num2 in &input[index1..] {
+            if num1 + num2 != 2020 { continue; }
+            println!("{} + {} = {}", num1, num2, num1 + num2);
+            println!("{} * {} = {}", num1, num2, num1 * num2);
+        }
+    }
+}
+
+fn part2(input: &Vec<u32>) {
+    for (index1, num1) in input.iter().enumerate() {
+        for (index2, num2) in &mut input[index1..].iter().enumerate() {
+            for num3 in &input[(index1+index2)..] {
+                if num1 + num2 + num3 != 2020 { continue; }
+                println!("{} + {} + {} = {}", num1, num2, num3, num1 + num2 + num3);
+                println!("{} * {} * {} = {}", num1, num2, num3, num1 * num2 * num3);
+            }
+        }
+    }
+}
---
+++ src/day2.rs
@@ -0,0 +1,51 @@
+extern crate regex;
+
+use regex::Regex;
+
+pub fn run() {
+    let re = Regex::new(r"^(\d{1,2})-(\d{1,2}) ([a-z]): ([a-z]+)$").unwrap();
+
+    println!("--- DAY 02 ---");
+    let input = include_str!("resources/input02.txt")
+        .split('\n')
+        .map(|policy| {
+            let caps = re.captures(policy).unwrap();
+            (
+                caps.get(1).unwrap().as_str().parse::<u8>().unwrap(),
+                caps.get(2).unwrap().as_str().parse::<u8>().unwrap(),
+                caps.get(3).unwrap().as_str().parse::<char>().unwrap(),
+                caps.get(4).unwrap().as_str(),
+            )
+        })
+        .collect::<Vec<(u8, u8, char, &str)>>();
+    part1(&input);
+    println!();
+    part2(&input);
+    println!();
+}
+
+fn part1(input: &Vec<(u8, u8, char, &str)>) {
+    println!(
+        "{}",
+        input
+            .iter()
+            .filter(|policy| {
+                (policy.0..=policy.1)
+                    .contains(&(policy.3.chars().filter(|char| char == &policy.2).count() as u8))
+            })
+            .count()
+    )
+}
+
+fn part2(input: &Vec<(u8, u8, char, &str)>) {
+    println!(
+        "{}",
+        input
+            .iter()
+            .filter(|policy| {
+                (policy.3.chars().nth((policy.0 - 1) as usize).unwrap() == policy.2)
+                    ^ (policy.3.chars().nth((policy.1 - 1) as usize).unwrap() == policy.2)
+            })
+            .count()
+    )
+}
